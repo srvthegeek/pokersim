0c05126b526cd1494b2e2fa3f153574b200c2c93
srvthegeek <61279639+srvthegeek@users.noreply.github.com>
2020-06-05 17:27:04 +0530

    added basic rating

---
+++ redirect.js
@@ -0,0 +1,9 @@
+function redirect() {
+
+    if (counter == 0) {
+    window.location.href="lobby1.html";
+    }
+    else if (counter > 0) {
+    window.location.href="lobbysim1.html"   
+    }
+}
--- showresult.js
+++ showresult.js
@@ -2,6 +2,9 @@ function showresult(){
     var x = document.getElementById("showsimresults");
     if (x.style.display === "none") {
     x.style.display = "block";
+    console.log(counter);
+    counter=counter+1;
+    console.log(counter);
     }
     
 }
