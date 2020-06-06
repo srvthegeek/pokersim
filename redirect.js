5a0ef96f4b3c25c61b3aa2f3890741b1486ed046
srvthegeek <61279639+srvthegeek@users.noreply.github.com>
2020-06-06 16:09:30 +0530

    added 5 players

--- redirect.js
+++ redirect.js
@@ -7,3 +7,43 @@ function redirect() {
     window.location.href="lobbysim1.html"   
     }
 }
+
+function redirect2() {
+
+    if (counter == 0) {
+    window.location.href="lobby2.html";
+    }
+    else if (counter > 0) {
+    window.location.href="lobbysim2.html"   
+    }
+}
+
+function redirect3() {
+
+    if (counter == 0) {
+    window.location.href="lobby3.html";
+    }
+    else if (counter > 0) {
+    window.location.href="lobbysim3.html"   
+    }
+}
+
+function redirect4() {
+
+    if (counter == 0) {
+    window.location.href="lobby4.html";
+    }
+    else if (counter > 0) {
+    window.location.href="lobbysim4.html"   
+    }
+}
+
+function redirect5() {
+
+    if (counter == 0) {
+    window.location.href="lobby5.html";
+    }
+    else if (counter > 0) {
+    window.location.href="lobbysim5.html"   
+    }
+}
